53b6daa366309ab2db5c514548913b7163dcc0eb
Lu <zlc140@163.com>
2017-09-05 13:51:49 +0800

    dev

--- config/index.js
+++ config/index.js
@@ -29,7 +29,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/upload':'http://192.168.0.9:8090//imageBase',
-
+      
       '/cart' : 'http://192.168.0.17:8080/fresh-order',
       '/makeOrder':'http://192.168.0.17:8080/fresh-order',
       '/dayOrder':'http://192.168.0.17:8080/fresh-order',
